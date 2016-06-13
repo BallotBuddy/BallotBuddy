941a06abf25ddc0ae1c19d6ac471a6e1ef595557
mjdubose <matthew.dubose@gmail.com>
2016-06-13 10:21:57 -0500

    added list of states

--- server/database/worker/dbgrabber.js
+++ server/database/worker/dbgrabber.js
@@ -37,6 +37,9 @@ var worker = function() {
     })
 }()
 
+
+//["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE",
+//"NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 //
 // Fetches a scrubbed array of candidates and
 // inserts them into the candidates table
