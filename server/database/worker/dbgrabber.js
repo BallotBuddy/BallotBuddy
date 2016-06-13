e7712c847ef13ccc131254882d5f84b269f0646d
mjdubose <matthew.dubose@gmail.com>
2016-06-13 11:20:00 -0500

    pulling all states. have not adjusted for picture url or state they represent

--- server/database/worker/dbgrabber.js
+++ server/database/worker/dbgrabber.js
@@ -7,7 +7,7 @@
 //var cfg = require('../config');
 var db = require('../database/db');
 var _ = require('underscore');
-var cand= require('./dbhelper');
+var cand = require('./dbhelper');
 var PromiseThrottle = require('promise-throttle');
 
 //
@@ -25,31 +25,49 @@ var pthrottle = new PromiseThrottle({
 // Main worker function collects candidates
 // then stores them in the database
 //
-var worker = function() {
+var states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE",
+  "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+
+var collectCandidates = function (states) {
+  return Promise.all(_.map(states, function (state) {
+    console.log(state);
+    return pthrottle.add(collectCandidate.bind(this, state))
+      .then(function () {
+        console.log("Candidate queried");
+      })
+  }))
+    .then(function () {
+      console.log("All candidates queried");
+    })
+}
+
+var worker = function () {
   db.deleteEverything();
   db.ensureSchema()
     .then(function () {
+
       console.log("Regenerated candidate tables");
-      return collectCandidates();
+      return collectCandidates(states);
     })
     .catch(function (err) {
       console.log("Worker failed: ", err);
     })
-}()
+} ();
 
 
-//["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE",
-//"NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+
+
+
 //
 // Fetches a scrubbed array of candidates and
 // inserts them into the candidates table
 //
-var collectCandidates = function() {
+var collectCandidate = function (state) {
   //
   // Query info for Candidate Search API
   //
-  var candidates= {
-    uri: 'http://www.opensecrets.org/api/?method=getLegislators&id=NJ&output=json',
+  var candidates = {
+    uri: 'http://www.opensecrets.org/api/?method=getLegislators&id=' + state + '&output=json',
     qs: {
       apikey: '31779c7d2d96d53b00c738b77b0d32a1'
 
