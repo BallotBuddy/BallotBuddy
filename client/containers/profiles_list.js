b898665863b3863cff25e922094c5e2c08387f37
James Lee <jameslee.atx@gmail.com>
2016-06-14 16:10:15 -0500

    profile tile styling

--- client/containers/profiles_list.js
+++ client/containers/profiles_list.js
@@ -6,19 +6,34 @@ class ProfileList extends Component {
   
   // builds the individual candidate tile (photo + name + party)
   renderProfile(profileData){
+    let logo = '';
     const name = profileData.candidate_firstlast;
     const picture = profileData.picture;
-    const affiliation = profileData.party + ', ' + profileData.office.slice(0, 2);
-    let divStyles = {
-      backgroundImage: 'url(' + picture + ')'
-    };
+    const rep = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Republicanlogo.svg/2000px-Republicanlogo.svg.png';
+    const dem = 'http://d3n8a8pro7vhmx.cloudfront.net/dplac/sites/1/meta_images/original/dem-donkey-right-copy.png?1413244000';
+    const state = profileData.state;
+    const title = profileData.office[2] === 'S' ? 'Sen.' : 'Rep.';
+    const partyStyle = {};
+    if (profileData.party === 'R') {
+      partyStyle['backgroundColor'] = 'rgba(141,0,36, .8)';
+      logo = rep;
+    }
+    if (profileData.party === 'D') {
+      partyStyle['backgroundColor'] = 'rgba(0,32,70, .8)';
+      logo = dem;
+    }
 
     return (
       <div className="profile-tile" key={profileData.candidate_id}>
-        <div className="profile-tile-image" style={divStyles} >
+        <div>
+          <img className="profile-picture" src={picture} />
         </div>
-        <div className="tile-detail">
-          {name} {affiliation}
+        <div className="tile-detail" style={partyStyle}>
+          <div className="candidate-name">{name}</div>
+          <div className="candidate-affiliation">
+            <img className="party-logo" src={logo} />
+            {title} {state}
+          </div>
         </div>
       </div>
     );
