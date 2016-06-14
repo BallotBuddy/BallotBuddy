b66f080055232af646026ccc0e9ee608d8acf00e
James Lee <jameslee.atx@gmail.com>
2016-06-14 10:36:54 -0500

    data from hardcoded server response populating as individual profiles - minor sytling additions

--- client/actions/index.js
+++ client/actions/index.js
@@ -47,10 +47,71 @@ export function fetchProfile(){
     }
   }
 
-  console.log('Heres the hard coded response:', hardCodedResponse)
+  const doubleResponse = {
+  "response": {
+    "legislator": [
+      {
+        "@attributes": {
+          "photo": 'https://s3.amazonaws.com/assets.opensecrets.org/politicians/img/N00033085.jpg',
+          "cid": "N00026148",
+          "firstlast": "Louis B Gohmert Jr",
+          "lastname": "GOHMERT",
+          "party": "R",
+          "office": "TX01",
+          "gender": "M",
+          "first_elected": "2004",
+          "exit_code": "0",
+          "comments": "",
+          "phone": "202-225-3035",
+          "fax": "202-226-1230",
+          "website": "http://gohmert.house.gov",
+          "webform": "http://gohmert.house.gov/contact/",
+          "congress_office": "2243 Rayburn House Office Building",
+          "bioguide_id": "G000552",
+          "votesmart_id": "50029",
+          "feccandid": "H4TX04039",
+          "twitter_id": "RepLouieGohmert",
+          "youtube_url": "http://youtube.com/GohmertTX01",
+          "facebook_id": "pages/foo/50375006903",
+          "birthdate": "1953-08-18"
+        }
+      },
+      {
+        "@attributes": {
+          "photo": 'https://s3.amazonaws.com/assets.opensecrets.org/politicians/img/N00033085.jpg',
+          "cid": "N00026457",
+          "firstlast": "Ted Poe",
+          "lastname": "POE",
+          "party": "R",
+          "office": "TX02",
+          "gender": "M",
+          "first_elected": "2004",
+          "exit_code": "0",
+          "comments": "",
+          "phone": "202-225-6565",
+          "fax": "202-225-5547",
+          "website": "http://poe.house.gov",
+          "webform": "https://poe.house.gov/index.cfm/contact-form#form_D05B44B5-B782-4DC7-BED6-FF0C90951FFF",
+          "congress_office": "2412 Rayburn House Office Building",
+          "bioguide_id": "P000592",
+          "votesmart_id": "49198",
+          "feccandid": "H4TX02108",
+          "twitter_id": "JudgeTedPoe",
+          "youtube_url": "http://youtube.com/CongressmanTedPoe",
+          "facebook_id": "pages/foo/106631626049851",
+          "birthdate": "1948-09-10"
+        }
+      }
+      ]
+    }
+   }
+
+  console.log('Heres the hard coded response:', hardCodedResponse);
+  console.log('Heres the hard coded response:', doubleResponse);
+
 
   return {
     type: FETCH_PROFILE,
-    payload: hardCodedResponse
+    payload: doubleResponse
   };
 }
