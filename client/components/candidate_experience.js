68ab6c2c0fa2162bba26f0a3de35c71ec844a33f
Ryan Newton <Ryan Newton>
2016-06-17 15:25:00 -0500

    Shell for candidate experience component

---
+++ client/components/candidate_experience.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class CandidateExperience extends Component {
+	
+	renderCandidateExperience(){ 		
+			<div className="candidate-experience">
+				<div>
+				</div>
+			</div>
+	}
+
+	render() {
+		return (
+      <div>
+      	{this.renderCandidateExperience()}
+    	</div>
+    );
+		)
+	}
+}
