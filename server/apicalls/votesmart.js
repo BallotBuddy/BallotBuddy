1caae7db76781d9e413e669482b2555dd00ed52e
mjdubose <matthew.dubose@gmail.com>
2016-06-18 11:27:57 -0500

    fixed candidate picture broken url issue

--- server/apicalls/votesmart.js
+++ server/apicalls/votesmart.js
@@ -1,9 +1,41 @@
 var rp = require('request-promise');
 var _ = require('underscore');
-
+var Promise = require("bluebird");
 
 
 var votesmart = module.exports;
+
+
+votesmart.checkPhoto = function (url) {
+  var photo = {
+    uri: url,
+    qs: {
+
+    },
+    headers: { 'User-Agent': 'request-promise' },
+
+  };
+  return votesmart.checkPhotoStatus(photo);
+
+}
+
+votesmart.checkPhotoStatus = function (photo) {
+  return rp(photo)
+    .then(function (res) {
+      return true;
+    })
+    .catch(function (err) {
+      return false;
+    })
+}
+
+
+// Promise.map(fileNames, function(fileName) {
+//     // Promise.map awaits for returned promises as well.
+//     return fs.readFileAsync(fileName);
+// }).then(function() {
+//     console.log("done");
+// });
 
 votesmart.collectCandidatesByZip = function (zip) {
   //
@@ -14,7 +46,7 @@ votesmart.collectCandidatesByZip = function (zip) {
     qs: {
       key: 'f4f8f4595fdf4d3773aee9e02da81f2b'                      //process.env.Candidate_key
     },
-    headers: {'User-Agent': 'request-promise'},
+    headers: { 'User-Agent': 'request-promise' },
     json: true,
   };
   return votesmart.fetch(candidates);
@@ -31,25 +63,33 @@ votesmart.fetch = function (request) {
     })
     .then(function (jsres) {
       var candidateList = jsres.candidateList.candidate;
-      candidateList = candidateList.map(function (candidate) {
-        candidate.picture = 'http://d229l5sflpl9cp.cloudfront.net/canphoto/' + candidate.candidateId + '.jg';
-        return candidate;
-      });   
-
+      candidateList = Promise.map(candidateList,function (candidate) {
+          var pictureUrl = 'http://d229l5sflpl9cp.cloudfront.net/canphoto/' + candidate.candidateId + '.jpg';
+          return votesmart.checkPhoto(pictureUrl)
+            .then(function (result) {
+              if (result) {
+                candidate.picture = pictureUrl;
+              }
+              else {
+                candidate.picture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUJwKOmAFsBQJdt7ohopn5GJ4s6SXft9ovC-fhU9QuRZABGNwt';
+              }
+              return candidate;
+            })
+        });
       return candidateList;
-
     });
 }
 
-votesmart.collectCandidateDetails = function(candid){
- var options= {
-    uri: 'http://api.votesmart.org/CandidateBio.getBio?candidateId='+candid +'&o=JSON',
+
+
+votesmart.collectCandidateDetails = function (candid) {
+  var options = {
+    uri: 'http://api.votesmart.org/CandidateBio.getBio?candidateId=' + candid + '&o=JSON',
     qs: {
-       key: 'f4f8f4595fdf4d3773aee9e02da81f2b',
-  //     options: 'JSON'                      //process.env.Candidate_key
-     },
-    headers: {'User-Agent': 'request-promise' },
-   json: true,
+      key: 'f4f8f4595fdf4d3773aee9e02da81f2b',  //process.env.Candidate_key
+    },
+    headers: { 'User-Agent': 'request-promise' },
+    json: true,
   };
   return votesmart.fetchBio(options);
 }
@@ -63,13 +103,13 @@ votesmart.fetchBio = function (request) {
     .catch(function (err) {
       console.log("Failed to fetch candidate bio info: ", err.message);
     })
-    .then(function (candidate) {   
-  candidate = candidate.bio;
+    .then(function (candidate) {
+      candidate = candidate.bio;
 
-  var returnedCandidate ={};
-    returnedCandidate.candidate = candidate.candidate;
-    returnedCandidate.election = candidate.election;    
-    console.log(returnedCandidate)  ;   
+      var returnedCandidate = {};
+      returnedCandidate.candidate = candidate.candidate;
+      returnedCandidate.election = candidate.election;
+      //  console.log(returnedCandidate);
       return returnedCandidate;
     });
 }
