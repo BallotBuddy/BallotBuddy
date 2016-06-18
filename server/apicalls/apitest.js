865b46e1ded1735bc0cecb2b27b9c7f08eb4bf47
mjdubose <matthew.dubose@gmail.com>
2016-06-18 14:44:18 -0500

    added in more votesmart api calls.. candidate search by last name, candidate office address, candidate email, contact information, etc.

--- server/apicalls/apitest.js
+++ server/apicalls/apitest.js
@@ -2,3 +2,6 @@ var vs = require('./votesmart');
 
 vs.collectCandidatesByZip('78704');
 vs.collectCandidateDetails(15723);
+vs.getCandidateCampaignAddress(15723);
+vs.getCandidateWebAddress(15723);
+vs.getCandidatesByLastName('Trump');
--- server/apicalls/votesmart.js
+++ server/apicalls/votesmart.js
@@ -29,18 +29,25 @@ votesmart.checkPhotoStatus = function (photo) {
     })
 }
 
+votesmart.getCandidatesByLastName=function(name){
+console.log ('Called getCandidatesByLastName');
+ var candidates = {
+    uri: 'http://api.votesmart.org/Candidates.getByLastname?lastName=' + name + '&o=JSON',
+    qs: {
+      key: 'f4f8f4595fdf4d3773aee9e02da81f2b'                      //process.env.Candidate_key
+    },
+    headers: { 'User-Agent': 'request-promise' },
+    json: true,
+  };
 
-// Promise.map(fileNames, function(fileName) {
-//     // Promise.map awaits for returned promises as well.
-//     return fs.readFileAsync(fileName);
-// }).then(function() {
-//     console.log("done");
-// });
+  return votesmart.fetch(candidates);
+}
 
+//
+// Query info for Votesmart API = get candidates by zip
+//
 votesmart.collectCandidatesByZip = function (zip) {
-  //
-  // Query info for Votesmart API = get candidates by zip
-  //
+
   var candidates = {
     uri: 'http://api.votesmart.org/Candidates.getByZip?zip5=' + zip + '&o=JSON',
     qs: {
@@ -63,23 +70,22 @@ votesmart.fetch = function (request) {
     })
     .then(function (jsres) {
       var candidateList = jsres.candidateList.candidate;
-      candidateList = Promise.map(candidateList,function (candidate) {
-          var pictureUrl = 'http://d229l5sflpl9cp.cloudfront.net/canphoto/' + candidate.candidateId + '.jpg';
-          return votesmart.checkPhoto(pictureUrl)
-            .then(function (result) {
-              if (result) {
-                candidate.picture = pictureUrl;
-              }
-              else {
-                candidate.picture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUJwKOmAFsBQJdt7ohopn5GJ4s6SXft9ovC-fhU9QuRZABGNwt';
-              }
-              return candidate;
-            })
-        });
+       candidateList = Promise.map(candidateList, function (candidate) {
+        var pictureUrl = 'http://d229l5sflpl9cp.cloudfront.net/canphoto/' + candidate.candidateId + '.jpg';
+        return votesmart.checkPhoto(pictureUrl)
+          .then(function (result) {
+            if (result) {
+              candidate.picture = pictureUrl;
+            }
+            else {
+              candidate.picture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUJwKOmAFsBQJdt7ohopn5GJ4s6SXft9ovC-fhU9QuRZABGNwt';
+            }
+            return candidate;
+          })
+      });
       return candidateList;
     });
 }
-
 
 
 votesmart.collectCandidateDetails = function (candid) {
@@ -114,3 +120,51 @@ votesmart.fetchBio = function (request) {
     });
 }
 
+
+votesmart.getCandidateCampaignAddress = function (candid) {
+  var options = {
+    uri: 'http://api.votesmart.org/Address.getCampaign?candidateId=' + candid + '&o=JSON',
+    qs: {
+      key: 'f4f8f4595fdf4d3773aee9e02da81f2b',  //process.env.Candidate_key
+    },
+    headers: { 'User-Agent': 'request-promise' },
+    json: true,
+  };
+  return votesmart.fetchCandidateCampaign(options);
+}
+
+
+
+votesmart.fetchCandidateCampaign = function (option) {
+  return rp(option)
+    .then(function (res) {
+      console.log("Successfully fetched candidate campaign addresses info");
+      return res;
+    })
+    .catch(function (err) {
+      console.log("Failed to fetch candidate campaign addresses info: ", err.message);
+    })
+}
+
+votesmart.getCandidateWebAddress = function(candid){
+  var options = {
+    uri: 'http://api.votesmart.org/Address.getOfficeWebAddress?candidateId=' + candid + '&o=JSON',
+    qs: {
+      key: 'f4f8f4595fdf4d3773aee9e02da81f2b',  //process.env.Candidate_key
+    },
+    headers: { 'User-Agent': 'request-promise' },
+    json: true,
+  };
+  return votesmart.fetchCandidateCampaign(options);
+}
+
+votesmart.fetchCandidateWebAddress = function(option){
+ return rp(option)
+    .then(function (res) {
+      console.log("Successfully fetched candidate office web address info");
+      return res;
+    })
+    .catch(function (err) {
+      console.log("Failed to fetch candidate office web address info: ", err.message);
+    })
+}
