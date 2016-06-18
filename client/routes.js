4cb3e95ea3ee0c547181ba9d924eb41facf3f9ba
jackjhall22 <jackjhall22@me.com>
2016-06-17 19:54:11 -0500

    add a route named jack to build a vid`eo component

--- client/routes.js
+++ client/routes.js
@@ -6,11 +6,14 @@ import App from './components/app';
 import ProfilesList from './containers/profiles_list';
 import SearchBar from './containers/search_bar';
 import DetailedProfile from './components/detailed_profile';
+import CandidateVideo from './components/candidate_video';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={SearchBar} />
     <Route path="profile/:cid" component={DetailedProfile}>
+    </Route>
+    <Route path="jack" component={CandidateVideo}>
     </Route>
   </Route>
 );
