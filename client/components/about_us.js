3d339bbd73a3fda29be8e002cd6c038d847de6c7
Ryan Newton <Ryan Newton>
2016-06-23 16:28:58 -0500

    Creates initial about_us component

---
+++ client/components/about_us.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export default const AboutUs = () => {
+	<div className="about_us">
+		<h1>About Us</h1>
+		<div className="about_project">
+			<h2>Project</h2>
+			<div>Ballotbuddy started as a class project, with the mission of making political information easily accessible for all voters.</div>
+		</div>
+		<div className="about_team">
+			<h2>Team</h2>
+			<div className="profile jack">
+				<h3>Jack Hall</h3>
+				<img src="https://c8.staticflickr.com/8/7406/27786958311_ec67890a0f_m.jpg" alt="Jack Hall">
+				<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<ul className="contact_info list-unstyled list-inline">
+					<li><a href="https://www.linkedin.com/in/rynewton">LinkedIn</a></li>
+					<li><a href="https://github.com/ryannewton/">Github</a></li>
+				</ul>
+			</div>
+			<div className="profile james">
+				<h3>James Lee</h3>
+				<img src="https://c6.staticflickr.com/8/7354/27786959261_924e6e9214.jpg" alt="James Lee">
+				<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<ul className="contact_info list-unstyled list-inline">
+					<li><a href="https://www.linkedin.com/in/rynewton">LinkedIn</a></li>
+					<li><a href="https://github.com/ryannewton/">Github</a></li>
+				</ul>
+			</div>
+			<div className="profile matt">
+				<h3>Matt Dubose</h3>
+				<img src="https://c4.staticflickr.com/8/7641/27786959651_c378e65eb3_m.jpg" alt="Matt Dubose">
+				<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<ul className="contact_info list-unstyled list-inline">
+					<li><a href="https://www.linkedin.com/in/rynewton">LinkedIn</a></li>
+					<li><a href="https://github.com/ryannewton/">Github</a></li>
+				</ul>
+			</div>
+			<div className="profile ryan">
+				<h3>Ryan Newton</h3>
+				<img src="https://c4.staticflickr.com/8/7056/27786958611_00789dc993_z.jpg" alt="Ryan Newton">
+				<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<ul className="contact_info list-unstyled list-inline">
+					<li><a href="https://www.linkedin.com/in/rynewton">LinkedIn</a></li>
+					<li><a href="https://github.com/ryannewton/">Github</a></li>
+				</ul>
+			</div>
+		</div>
+		<div className="about_sources">
+			<h2>Sources</h2>
+			<div>VoteSmart API</div>
+			<div>OpenSecrets API</div>
+		</div>
+	</div>
+}
