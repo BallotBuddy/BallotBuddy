b5a95c54e15c015ba045f2e7db96f81b82d87059
Ryan Newton <Ryan Newton>
2016-06-23 16:28:23 -0500

    Adds route for /aboutus page

--- client/routes.js
+++ client/routes.js
@@ -6,13 +6,8 @@ import App from './components/app';
 import ProfilesList from './containers/profiles_list';
 import SearchBar from './containers/search_bar';
 import DetailedProfile from './components/detailed_profile';
-<<<<<<< 278bde5bb5b331bd089fc05d5164abbc54badaab
 import CandidateVideo from './components/candidate_video';
 import AboutUs from './components/about_us';
-=======
-import AboutUs from './components/about_us';
-import CandidateVideo from './components/candidate_video';
->>>>>>> refactor to display just the video player
 
 export default (
   <Route path="/" component={App}>
@@ -20,11 +15,5 @@ export default (
     <Route path="profile/:cid" component={DetailedProfile}>
     </Route>
     <Route path="aboutus" component={AboutUs}></Route>
-<<<<<<< 278bde5bb5b331bd089fc05d5164abbc54badaab
-    <Route path="jack" component={CandidateVideo}>
-=======
-    <Route path="profile/:cid" component={CandidateVideo}>
->>>>>>> refactor to display just the video player
-    </Route>
   </Route>
 );
