bd97fdb8d5767d1afb4066d60c0e1684eb5992cc
Ryan Newton <Ryan Newton>
2016-06-23 16:28:23 -0500

    Adds route for /aboutus page

--- client/routes.js
+++ client/routes.js
@@ -7,11 +7,13 @@ import ProfilesList from './containers/profiles_list';
 import SearchBar from './containers/search_bar';
 import DetailedProfile from './components/detailed_profile';
 import CandidateVideo from './components/candidate_video';
+import AboutUs from './components/about_us';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={SearchBar} />
     <Route path="profile/:cid" component={DetailedProfile}>
     </Route>
+    <Route path="aboutus" component={AboutUs} />
   </Route>
 );
