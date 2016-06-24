1652e551d4e845069f8feceb29fe33856f2efc82
jackjhall22 <jackjhall22@me.com>
2016-06-24 11:26:53 -0500

    refactor youtube api keys and delete console.logs

--- client/components/candidate_video.js
+++ client/components/candidate_video.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
 import DetailedProfile from './detailed_profile'
 import YTSearch from 'youtube-api-search';
-var api_keys = require('../../api_keys');
-var api_key = api_keys.YOUTUBE_API;
+const api_keys = require('../../api_keys');
+const api_key = api_keys.YOUTUBE_API;
 const API_KEY = process.env.Youtube_Key || api_key;
 
 class CandidateVideo extends Component {
@@ -15,14 +13,15 @@ class CandidateVideo extends Component {
 			videos: [],
 			selectedVideo: null
 	  };
-		this.CandidateVideofetch();
+	}
+
+	componentWillMount(){
+		this.fetchCandidateVideo();	
 	}
 	
-	CandidateVideofetch() {
+	fetchCandidateVideo() {
 		const name = this.props.candInfo.ballotName;
-		console.log("name", name);
-		YTSearch({key: API_KEY, term: name+" Official Campaign Video" }, (videos) => {
-			console.log(videos)
+		YTSearch({key: API_KEY, term: `${name}Official Campaign Video` }, (videos) => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
@@ -31,7 +30,6 @@ class CandidateVideo extends Component {
 	}
 
 	renderCandidatePlayer() {
-		console.log("video in render CandidatePlayer", this.state.selectedVideo);
 		if (!this.state.selectedVideo) {
 			return <div>Loading...</div>;
 		}
@@ -52,7 +50,6 @@ class CandidateVideo extends Component {
 	);
 };
 
-
 	render() {
 		return (
 			<div className = "video">
@@ -62,10 +59,4 @@ class CandidateVideo extends Component {
 	}
 }
 
-
 export default CandidateVideo;
-// function mapStateToProps(state) {
-// 	return { voteSmartBio: state.profiles.voteSmartBio };
-// }
-
-//export default connect(mapStateToProps )(CandidateVideo);
