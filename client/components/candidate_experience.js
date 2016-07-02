aaadce1911044d6017144afc6093e70a968f0d5b
Ryan Newton <newton1988@gmail.com>
2016-07-02 15:04:23 -0500

    Cleans up candidate_experience.js

--- client/components/candidate_experience.js
+++ client/components/candidate_experience.js
@@ -3,22 +3,25 @@ import React, { Component } from 'react';
 export default class CandidateExperience extends Component {
 
 	renderCandidateExperience() {
-		const sectionObjs = [];
-		sectionObjs.push({category: "Education", data:this.props.candInfo.education});
-		sectionObjs.push({category: "Professional", data:this.props.candInfo.profession});
-		sectionObjs.push({category: "Political", data:this.props.candInfo.political});
-		sectionObjs.push({category: "Congressional", data:this.props.candInfo.congMembership});
-		sectionObjs.push({category: "Other", data:this.props.candInfo.orgMembership});
+		const expObjs = [
+			{category: 'Education',			data:this.props.candInfo.Education			},
+			{category: 'Professional',	data:this.props.candInfo.Professional		},
+			{category: 'Political',			data:this.props.candInfo.Political			},
+			{category: 'Congressional',	data:this.props.candInfo.congMembership	},
+			{category: 'Other',					data:this.props.candInfo.orgMembership	}
+		];
+
 		return (
 			<table className="candidate-experience-table">
 				<tbody className="candidate-table-body">
-					{ sectionObjs.map((obj) => {
+					{/* Maps through each experience category and builds a table row */}
+					{ expObjs.map((obj) => {
 						return (
 							<tr className="table-row-category" key={obj.category}>
 								<td className="experience-category">{obj.category}:</td>
 								<td className="experience-details">
 									<ul className="list-unstyled">
-										{/*Puts each newline on it's own bullet*/}
+										{/* Puts each newline on it's own bullet */}
 										{ obj.data.split('\n').map(line => { return <li className="experience-item" key={line}>{line}</li> }) }
 									</ul>
 								</td>
