ecd42780c08615162dab217bcdd00bde2b14469a
jackjhall22 <jackjhall22@me.com>
2016-07-02 15:35:29 -0500

    fix the courage survey message remove the the <br></br> from the text

--- client/containers/candidate_courage.js
+++ client/containers/candidate_courage.js
@@ -15,8 +15,13 @@ class CandidateCourage extends Component {
     }
     const response = this.props.courage;
     const questions = response.section;
+    // remove the <br> tags in the survey message;
+    let surveyMessage = response.surveyMessage;
+      surveyMessage = surveyMessage.split('<');
+      surveyMessage = surveyMessage[0];
+
     if (!questions) {
-      return <div className="courage-topic">{response.surveyMessage}</div>
+      return <div className="courage-topic">{surveyMessage}</div>
     }
       return questions.map((topic) => {
         return (() => {
